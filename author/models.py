59f0898578a90ebc01fa975cd1ed65ae46085e85
MiKPo4eLiK <stassojcuk@gmail.com>
2025-09-01 19:48:16 +0300

    well

--- author/models.py
+++ author/models.py
@@ -7,3 +7,11 @@ class Author(models.Model):
     pseudonym = models.CharField(max_length=64, null=True, blank=True)
     age = models.IntegerField()
     retired = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.pseudonym if self.pseudonym else f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
